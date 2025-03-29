6ff6021317855cbd4981ecd075a4f29eea08c30d
Cardsea <cardiff.emde@gmail.com>
2025-03-29 12:13:53 -0700

    Fix for python3

--- textPlayer.py
+++ textPlayer.py
@@ -2,7 +2,7 @@ import os, sys, signal, time, re
 from signal import signal, SIGPIPE, SIG_DFL
 from subprocess import PIPE, Popen
 from threading import Thread
-from Queue import Queue, Empty
+from queue import Queue, Empty
 
 '''
  Currently, for games that require several clicks to get start info, it doesn't scrape everything. Lost.z5 is one. The first couple commands will not produce the expected output.
@@ -23,9 +23,9 @@ class TextPlayer:
 		self.game_loaded_properly = True
 
 		# Verify that specified game file exists, else limit functionality
-		if game_filename == None or not os.path.exists('textplayer/games/' + game_filename):
+		if game_filename == None or not os.path.exists('games/' + game_filename):
 			self.game_loaded_properly = False
-			print "Unrecognized game file or bad path"
+			print("Unrecognized game file or bad path")
 			return
 
 		self.game_filename = game_filename
@@ -37,7 +37,7 @@ class TextPlayer:
 		if self.game_loaded_properly == True:
 
 			# Start the game process with both 'standard in' and 'standard out' pipes
-			self.game_process = Popen(['./textplayer/frotz/dfrotz', 'textplayer/games/' + self.game_filename], stdin=PIPE, stdout=PIPE, bufsize=1)
+			self.game_process = Popen(['./frotz/dfrotz', 'games/' + self.game_filename], stdin=PIPE, stdout=PIPE, bufsize=1)
 
 			# Create Queue object
 			self.output_queue = Queue()
@@ -59,7 +59,7 @@ class TextPlayer:
 	# Sends buffer from output pipe of game to a queue where it can be retrieved later
 	def enqueue_pipe_output(self, output, queue):
 		for line in iter(output.readline, b''):
-		    queue.put(line)
+			queue.put(line.decode('utf-8', errors='ignore'))
 		output.close()
 
 	# Run a bash command and wait until it finishes
@@ -70,26 +70,27 @@ class TextPlayer:
 	# Parses through a text list of commands (or a single command) and executes them
 	def parse_and_execute_command_file(self, input_filename):
 		if self.game_loaded_properly == True:
-			if (os.path.exists(filename)):
-				f = open(filename, 'r')
-				commands = f.read()
-				f.close()
-				if '\n' in commands:
-					for command in commands.split('\n'):
-						print self.execute_command(command)
-				else:
-					print self.execute_command(command)
+			if (os.path.exists(input_filename)):
+				with open(input_filename, 'r') as f:
+					commands = f.read()
+					if '\n' in commands:
+						for command in commands.split('\n'):
+							print(self.execute_command(command))
+					else:
+						print(self.execute_command(commands))
 
 	# Send a command to the game and return the output
 	def execute_command(self, command):
 		if self.game_loaded_properly == True:
-			self.game_process.stdin.write(command + '\n')
+			self.game_process.stdin.write((command + '\n').encode())
+			self.game_process.stdin.flush()
 			return self.clean_command_output(self.get_command_output())
 
 	# Returns the current score in a game
 	def get_score(self):
 		if self.game_loaded_properly == True:
-			self.game_process.stdin.write('score\n')
+			self.game_process.stdin.write(b'score\n')
+			self.game_process.stdin.flush()
 			command_output = self.get_command_output()
 			score_pattern = '[0-9]+ [\(total ]*[points ]*[out ]*of [a maximum of ]*[a possible ]*[0-9]+'
 			matchObj = re.search(score_pattern, command_output, re.M|re.I)
@@ -131,8 +132,11 @@ class TextPlayer:
 
 	def quit(self):
 		if self.game_loaded_properly == True:
-			self.game_process.stdin.write('quit\n')
-			self.game_process.stdin.write('y\n')
+			self.game_process.stdin.write(b'quit\n')
+			self.game_process.stdin.flush()
+			self.game_process.stdin.write(b'y\n')
+			self.game_process.stdin.flush()
 		if self.game_process.stdin != None:
-			self.game_process.stdin.write('n\n')
+			self.game_process.stdin.write(b'n\n')
+			self.game_process.stdin.flush()
 
